1894d2138d6c8fe84dcf11f7549f19dbca679649
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-10-21 13:10:48 +0200

    Added a Minimal Working Example

---
+++ samples/minimal.cpp
@@ -0,0 +1,53 @@
+#include <opencv2/videoio.hpp>
+#include <iostream>
+
+#include <chilitrack.h>
+
+using namespace std;
+using namespace cv;
+using namespace chilitrack;
+
+int main(int argc, char **argv)
+{
+    Mat frame;
+
+    namedWindow("tracking");
+
+    if(argc < 2) {
+        cerr << "Usage: " << endl <<
+                "tracker template_path" << endl;
+        return 1;
+    }
+    VideoCapture video_in(1);
+
+    if(!video_in.isOpened()) {
+        cerr << "Couldn't open camera" << endl;
+        return 1;
+    }
+
+    //Read the template
+    // ==================
+
+    Mat image_object = imread(argv[1], IMREAD_GRAYSCALE);
+    auto tpl = makePtr<Template>(Template(image_object,
+                                          Size(200, 297)));
+
+    // Create the tracker
+    // ==================
+
+    Tracker tracker;
+
+    // Main loop
+    // =========
+
+    while(true)
+    {
+        video_in >> frame;
+
+        tracker.process(frame, tpl);
+        imshow("tracking", tracker._debug);
+        if (waitKey(10) >= 0) break;
+    }
+
+    return 0;
+}
